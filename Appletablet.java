64e472a7f840e92d7148dc142f7edeaa73add8ed
Pedrix2002 <pdeleonc5@miumg.edu.gt>
2024-10-22 21:44:02 -0600

    Create Appletablet.java

---
+++ Appletablet.java
@@ -0,0 +1,9 @@
+package Phone;
+
+
+public class AppleTablet implements Tablet {
+
+ public void getSpecifications() {
+     System.out.println("Apple Tablet: iPadOS, A14 Chip, 10.9-inch display");
+ }
+}
