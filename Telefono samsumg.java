54ff78cdef0b14617a5c75d3303f08a1d5127da7
Pedrix2002 <pdeleonc5@miumg.edu.gt>
2024-10-22 21:47:49 -0600

    Create Telefono samsumg.java

---
+++ Telefono samsumg.java
@@ -0,0 +1,9 @@
+package Phone;
+
+
+public class SamsungPhone implements Phone {
+
+ public void getSpecifications() {
+     System.out.println("Samsung Phone: Android, Exynos 2100, 6.2-inch display");
+ }
+}
