e8c0e137c2d7cc2ab81713b170cb62722ef269b1
Pedrix2002 <pdeleonc5@miumg.edu.gt>
2024-10-22 21:48:18 -0600

    Create SamsungTablet.java

---
+++ Phone/src/Phone/SamsungTablet.java
@@ -0,0 +1,9 @@
+package Phone;
+
+
+public class SamsungTablet implements Tablet {
+
+ public void getSpecifications() {
+     System.out.println("Samsung Tablet: Android, Exynos 9611, 10.4-inch display");
+ }
+}
